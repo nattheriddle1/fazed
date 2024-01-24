820cef3aaaf591aa64c97a4a58305664cb7ec602
Nat Riddle <nriddle@choicesolutions.com>
2024-01-24 12:42:26 -0500

    Added linker flags

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -155,7 +155,7 @@
  #define JucePlugin_ARAFactoryID           "com.NatRiddle.Fazed.factory"
 #endif
 #ifndef  JucePlugin_ARADocumentArchiveID
- #define JucePlugin_ARADocumentArchiveID   "com.NatRiddle.Fazed.aradocumentarchive.0.3.0"
+ #define JucePlugin_ARADocumentArchiveID   "com.NatRiddle.Fazed.aradocumentarchive.1.0.0"
 #endif
 #ifndef  JucePlugin_ARACompatibleArchiveIDs
  #define JucePlugin_ARACompatibleArchiveIDs  ""
